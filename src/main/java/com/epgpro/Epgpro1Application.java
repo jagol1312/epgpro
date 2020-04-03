e4fd436ea666002de6113905250872bf1b949f2e
jagol1312 <354393530@qq.com>
2020-04-03 11:38:52 +0800

    first

---
+++ src/main/java/com/epgpro/Epgpro1Application.java
@@ -0,0 +1,13 @@
+package com.epgpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epgpro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epgpro1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/epgpro/control/Index.java
@@ -0,0 +1,19 @@
+package com.epgpro.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 
+ * @author Administrator
+ * @date 2020年4月3日上午11:14:50
+ * 描述：
+ */
+@Controller
+public class Index {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
